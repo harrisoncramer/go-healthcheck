1086c91c50d542ce6c1fcd758a2f8a230f81574f
Harrison Cramer <harrisoncramer@gmail.com>
2022-08-27 11:37:22 -0400

    Added endpoint option to jobs array

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -17,7 +18,7 @@ const usage = `
 ****************************************************************
 
  SYNOPSIS
-    go-healthcheck -f config_file.yml ...
+    go-healthcheck -f config_file.yml
   DESCRIPTION
      This is a script template
      to start any good shell script.
@@ -30,34 +31,57 @@ const usage = `
      go-healthcheck -f production-check.yml
 `
 
+const colorRed = "\033[31m"
+const colorYellow = "\033[33m"
+const colorWhite = "\033[37m"
+
 func check(e error) {
-	colorRed := "\033[31m"
 	if e != nil {
 		log.Print(usage)
 		log.Fatal(string(colorRed), e)
 	}
 }
 
+type Job struct {
+	Endpoint string
+}
+
 type Config struct {
 	Schedule int
 	Base_url string
+	Port     int
+	Jobs     []Job
 }
 
 func (o *Config) Init() {
 	if o.Base_url == "" {
-		log.Println("config.base_url not set, defaulting to 'http://localhost'")
+		log.Println(string(colorYellow), "config warning: config.base_url not set, defaulting to 'http://localhost'")
 		o.Base_url = "http://localhost"
+	}
+
+	if o.Port == 0 {
+		log.Println(string(colorYellow), "config warning: config.port not set, defaulting to 80")
+		o.Port = 80
 	}
 }
 
 var (
 	ErrorConfigNotProvided = "config error: Configuration file not provided"
 	ErrorScheduleNotSet    = "config error: config.schedule not set"
+	ErrorJobEndpointNotSet = func(i int) string {
+		return "config error: config.jobs[" + fmt.Sprintf("%v", i) + "] endpoint not set"
+	}
 )
 
 func validateConfig(config Config) error {
 	if config.Schedule == 0 {
 		return errors.New(ErrorScheduleNotSet)
+	}
+
+	for i, job := range config.Jobs {
+		if job.Endpoint == "" {
+			return errors.New(ErrorJobEndpointNotSet(i))
+		}
 	}
 
 	return nil
@@ -89,7 +113,7 @@ func main() {
 
 	s := gocron.NewScheduler(time.UTC)
 	s.Every(config.Schedule).Milliseconds().Do(func() {
-		log.Printf("Hello World!")
+		log.Println(string(colorWhite), "Running job...")
 	})
 
 	s.StartBlocking()
